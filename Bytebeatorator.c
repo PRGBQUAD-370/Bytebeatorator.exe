0810b4395274a495933e4fa6b4ebb54c1eb00a16
PRGBQUAD-370 <kobandl@icloud.com>
2026-06-16 01:47:00 -0500

    Add files via upload

---
+++ Bytebeatorator.c
@@ -0,0 +1,68 @@
+#include <Windows.h>
+#pragma comment(lib, "winmm.lib")
+
+#include "sound.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	sound1();
+	Sleep(30000);
+	sound2();
+	Sleep(30000);
+	sound3();
+	Sleep(30000);
+	sound4();
+	Sleep(30000);
+	sound5();
+	Sleep(30000);
+	sound6();
+	Sleep(30000);
+	sound7();
+	Sleep(47000);
+	sound8();
+	Sleep(30000);
+	sound9();
+	Sleep(30000);
+	sound10();
+	Sleep(30000);
+	sound11();
+	Sleep(30000);
+	sound12();
+	Sleep(30000);
+	sound13();
+	Sleep(30000);
+	sound14();
+	Sleep(30000);
+	sound15();
+	Sleep(30000);
+	sound16();
+	Sleep(30000);
+	sound17();
+	Sleep(30000);
+	sound18();
+	Sleep(30000);
+	sound19();
+	Sleep(30000);
+	sound20();
+	Sleep(30000);
+	sound21();
+	Sleep(30000);
+	sound22();
+	Sleep(30000);
+	sound23();
+	Sleep(30000);
+	sound24();
+	Sleep(30000);
+	sound25();
+	Sleep(30000);
+	sound26();
+	Sleep(30000);
+	sound27();
+	Sleep(30000);
+	sound28();
+	Sleep(30000);
+	sound29();
+	Sleep(30000);
+	sound30();
+	Sleep(30000);
+}
